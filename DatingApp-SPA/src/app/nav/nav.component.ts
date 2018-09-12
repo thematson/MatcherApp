a4658e771bdde00347e3805351589001c0d34b6a
Eric Matson <william.matson@superion.com>
2018-09-12 12:51:53 -0400

    added pseudo login/out

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => { // next == if succesful
+      console.log('Logged in successfully');
+    }, error => {
+      console.log('Failed to login');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token; // shorthand for if something's here, retunr true, otherwise return false
+  }
+
+  logOut() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+    
+  }
+
+}
