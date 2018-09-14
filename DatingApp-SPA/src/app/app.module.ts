f4b478d7eb43fedf3c899c7ece77b4619afd22c2
Eric Matson <william.matson@superion.com>
2018-09-14 07:42:53 -0400

    code cleanup

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,16 +4,18 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { HttpClient } from 'selenium-webdriver/http';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
@@ -21,7 +23,7 @@ import { AuthService } from './_services/auth.service';
       FormsModule
    ],
    providers: [
-       AuthService
+      AuthService
    ],
    bootstrap: [
       AppComponent
