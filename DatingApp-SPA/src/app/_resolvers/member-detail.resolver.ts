1827ece7fd3ce44a0dd0ed162b1a5d1f756b129f
Eric Matson <william.matson@superion.com>
2018-10-05 16:15:41 -0400

    resolver addition

--- DatingApp-SPA/src/app/_resolvers/member-detail.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-detail.resolver.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
 import { User } from '../_models/user';
+import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { Observable } from 'rxjs';
 
 @Injectable();
 export class MemberDetailREsolver implements Resolve<User> {
-    
+    constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
+
+    resolve(route: ActivatedRouteSnapshot): Observable<User> {
+        return 
+    }
 }
