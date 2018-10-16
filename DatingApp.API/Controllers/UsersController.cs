5fc346806bdc1a3bf99c6a82fd9d5114995e50a0
Eric Matson <william.matson@superion.com>
2018-10-16 15:29:21 -0400

    added update user method

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.API.Data;
@@ -13,13 +15,15 @@ namespace DatingApp.API.Controllers {
     public class UsersController : ControllerBase {
         private readonly IDatingRepository _repo;
         private readonly IMapper _mapper;
-        public UsersController (IDatingRepository repo, IMapper mapper) {
+        public UsersController (IDatingRepository repo, IMapper mapper) 
+        {
             _mapper = mapper;
             _repo = repo;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetUsers () {
+        public async Task<IActionResult> GetUsers () 
+        {
             var users = await _repo.GetUsers ();
 
             var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
@@ -27,13 +31,30 @@ namespace DatingApp.API.Controllers {
             return Ok (usersToReturn);
         }
 
-        [HttpGet ("{id}")]
+        [HttpGet("{id}")]
         public async Task<IActionResult> GetUser (int id) {
             var user = await _repo.GetUser (id);
 
             var userToReturn = _mapper.Map<UserForDetailedDto>(user);
 
             return Ok (userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser (int id, UserForUpdateDto userForUpdateDto) 
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+            var userFromRepo = await _repo.GetUser(id);
+
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if (await _repo.SaveAll())
+                return NoContent();
+
+            throw new Exception($"Updating user {id} failed on save");
         }
     }
 }
