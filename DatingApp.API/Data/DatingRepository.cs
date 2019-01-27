3e9fc90f31003059be2e00d50ac55c911195eb74
Eric Matson <eric@Erics-MacBook-Pro.local>
2019-01-27 09:10:47 -0500

    added photo crud

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -4,19 +4,21 @@ using System.Threading.Tasks;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace DatingApp.API.Data {
-    public class DatingRepository : IDatingRepository {
+namespace DatingApp.API.Data
+{
+    public class DatingRepository : IDatingRepository
+    {
         private readonly DataContext _context;
-        public DatingRepository (DataContext context) 
+        public DatingRepository(DataContext context)
         {
             _context = context;
         }
-        public void Add<T> (T entity) where T : class 
+        public void Add<T>(T entity) where T : class
         {
             _context.Add(entity);
         }
 
-        public void Delete<T> (T entity) where T : class 
+        public void Delete<T>(T entity) where T : class
         {
             _context.Remove(entity);
         }
@@ -34,21 +36,21 @@ namespace DatingApp.API.Data {
             return photo;
         }
 
-        public async Task<User> GetUser (int id) 
+        public async Task<User> GetUser(int id)
         {
             var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
 
             return user;
         }
 
-        public async Task<IEnumerable<User>> GetUsers () 
+        public async Task<IEnumerable<User>> GetUsers()
         {
             var users = await _context.Users.Include(p => p.Photos).ToListAsync();
 
             return users;
         }
 
-        public async Task<bool> SaveAll () 
+        public async Task<bool> SaveAll()
         {
             return await _context.SaveChangesAsync() > 0;
         }
