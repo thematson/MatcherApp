dbe4b9664f68c7d0b51378a341637f35e8bbfa32
Eric Matson <eric@Erics-MacBook-Pro.local>
2019-01-27 09:10:12 -0500

    added photo crud

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from './_services/auth.service';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { User } from './_models/user';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "./_services/auth.service";
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { User } from "./_models/user";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
   jwtHelper = new JwtHelperService();
@@ -14,13 +14,14 @@ export class AppComponent implements OnInit {
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    const token = localStorage.getItem('token');
-    const user: User = JSON.parse(localStorage.getItem('user'));
+    const token = localStorage.getItem("token");
+    const user: User = JSON.parse(localStorage.getItem("user"));
     if (token) {
       this.authService.decodedToken = this.jwtHelper.decodeToken(token);
     }
     if (user) {
       this.authService.currentUser = user;
+      this.authService.changeMemberPhoto(user.photoUrl);
     }
   }
 }
